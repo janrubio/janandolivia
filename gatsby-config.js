29534515df7023808dd1c379407db215e129a430
Jan Rubio <rubiojan@gmail.com>
2019-01-29 00:09:02 -0500

    Add placeholder layout

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    pathPrefix: "/janandolivia",
+    plugins: [
+
+    ],
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () =>
+    <Layout>
+        <div>
+            Jan & Olivia
+        </div>
+    </Layout>
