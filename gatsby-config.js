1585b0e5772732a8a490b710082f5a5dbc12be7b
Jan Rubio <rubiojan@gmail.com>
2019-01-29 00:16:02 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
     pathPrefix: "/janandolivia",
     plugins: [
-
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-45997711-10",
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+            },
+        },
     ],
 }
