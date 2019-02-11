7faa3495234c36bbbb2b12d9d88326f6cd7be76b
Jan Rubio <rubiojan@gmail.com>
2019-02-11 00:29:14 -0500

    Add logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Layout from "../components/layout"
 
 export default () =>
     <Layout>
-        <div>
-            Jan & Olivia
+        <div className="logo">
+            <div className="logo__container">
+                <img src="/janandolivia_logo.svg" alt="Logo"/>
+            </div>
         </div>
     </Layout>
