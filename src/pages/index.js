2b788f4fd5cc4ee273ae44a54b04fe9847b42fc7
Olivia Cheng <olivia@heyitsolivia.com>
2019-02-11 12:15:13 -0500

    Make banner responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ export default () =>
             <img className="logo" src="/janandolivia_logo.svg" alt="Jan and Olivia are getting married!"/>
             <div className="banner__detail-group flex-container">
                 <div className="banner__detail">
-                    Sunday, June 30th
+                    <span className="show-for-medium">Sunday, </span>June 30th
                 </div>
                 <div className="banner__detail">
-                    Queens, New York
+                    Queens, <span className="hide-for-medium">NY</span><span className="show-for-medium">New York</span>
                 </div>
             </div>
         </div>
