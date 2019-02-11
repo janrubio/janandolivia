630ed6ea40016a2628961459343e7d7330844644
Jan Rubio <rubiojan@gmail.com>
2019-02-11 00:33:46 -0500

    Add coming soon message

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default () =>
         <div className="logo">
             <div className="logo__container">
                 <img src="/janandolivia_logo.svg" alt="Logo"/>
+                <p className="logo__description">more details coming soon</p>
             </div>
         </div>
     </Layout>
